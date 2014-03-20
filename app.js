4a1f34ca541bb616780b1432cc70714cdc58f82d
Adrian Li <li.adrianmc@gmail.com>
2014-03-20 13:59:00 -0700

    finish adding code

--- app.js
+++ app.js
@@ -56,5 +56,10 @@ io.sockets.on('connection', function (socket) {
 
 	console.log('socket: server sends pong to all (3)');
 
+	socket.on( 'drawCircle', function( data, session ) {
+    	socket.broadcast.emit( 'drawCircle', data );
+	})
+
     });
 });
+
