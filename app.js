f2d66a2b0f7faeaa56ec559ed2e68d5c9ba8b35a
Adrian Li <li.adrianmc@gmail.com>
2014-03-20 13:42:42 -0700

    added socket.io

--- app.js
+++ app.js
@@ -30,6 +30,31 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+/*
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+*/
+
+var server = http.createServer(app).listen( app.get('port') );
+var io = require('socket.io').listen(server, function() {
+        console.log("Express server listening on port " + app.get('port'));
+});
+
+// A user connects to the server (opens a socket)
+io.sockets.on('connection', function (socket) {
+
+    // (2): The server recieves a ping event
+    // from the browser on this socket
+    socket.on('ping', function ( data ) {
+  
+    console.log('socket: server recieves ping (2)');
+
+	// (3): Emit a pong event all listening browsers
+	// with the data from the ping event
+	io.sockets.emit( 'pong', data );   
+
+	console.log('socket: server sends pong to all (3)');
+
+    });
 });
